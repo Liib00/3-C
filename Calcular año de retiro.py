d4377a4f9127fa9176955c0faf51251af69290a8
Liib00 <89849996+Liib00@users.noreply.github.com>
2021-09-28 08:39:00 -0500

    Update Calcular año de retiro.py

--- Calcular año de retiro.py
+++ Calcular año de retiro.py
@@ -8,3 +8,4 @@ Hacer años_para retiro = edad_de_retiro - edad_actual
 Hacer año_de_retiro = año_actual + año_para_retiro
 Mostrar mensaje "Tienes " años_para_retiro "para tu retiro"
 Mostrar mensaje "Es el año " año_actual "entonces tu retiro será el año " año_de_retiro
+Fin
