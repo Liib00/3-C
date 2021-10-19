2466848517097da4256f266ae52f6d06c0eaa335
LIBNI DANIELA DANIELA <89849996+Liib00@users.noreply.github.com>
2021-10-19 12:49:43 +0000

    pizza party

---
+++ pizza_algoritmo.py
@@ -0,0 +1,6 @@
+Inicio 
+Definir numero_de personas que comerán pizza = entrada del usuario
+Preguntar al usuario "¿Cuántas pizzas tiene?"
+Definir numero_de pizzas = entrada del usuario
+Preguntar al usuario "¿Cuántas rebanadas tiene la pizza?"
+Definir numero_de rebanadas que tiene la pizza = entrada del ususario
