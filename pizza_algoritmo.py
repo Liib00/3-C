a882cfc591295ba1b6f6cbe7e05054f00b793ae1
LIBNI DANIELA DANIELA <89849996+Liib00@users.noreply.github.com>
2021-11-02 18:49:18 +0000

    Hice el algoritmo para el programa pizza party

--- pizza_algoritmo.py
+++ pizza_algoritmo.py
@@ -1,6 +1,7 @@
 Inicio 
-Definir numero_de personas que comerán pizza = entrada del usuario
-Preguntar al usuario "¿Cuántas pizzas tiene?"
-Definir numero_de pizzas = entrada del usuario
-Preguntar al usuario "¿Cuántas rebanadas tiene la pizza?"
-Definir numero_de rebanadas que tiene la pizza = entrada del ususario
+Definir numero_de personas, numero de pizzas, numero de rebanadas
+Hacer numero de rebanadas totales = numero de pizzas * numero de rebanadas 
+Hacer numero de rebanadas por persona = numero de rebanadas totales / numero de personas
+Imprimir "numero de rebanadas por persona"
+Imprimir "cantidad de rebanadas que sobran"
+Fin
